a4b655ed2be867ccb4437003eb2d9765df7adad6
Mike <mfkuznetsov_1@edu.hse.ru>
2020-05-06 04:48:58 +0300

    Task4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 /*
  * На вход подается строка, состоящая из целых чисел типа int, разделенных одним или несколькими пробелами.
@@ -13,6 +15,7 @@
  * 
  * Пример выходных:
  * 8
+ * 
  * 8
  * 
  * Пояснение:
@@ -39,30 +42,75 @@ namespace Task04
 
         public static void RunTesk04()
         {
+            // Создадим массив.
             int[] arr;
             try
             {
                 // Попробуйте осуществить считывание целочисленного массива, записав это ОДНИМ ВЫРАЖЕНИЕМ.
-                arr = 
+                arr = (from s in Regex.Replace(Console.ReadLine(), "[ ]+", " ").Trim().Split()
+                       select int.Parse(s)).ToArray();
             }
-           
-                // использовать синтаксис методов! SQL-подобные запросы не писать!
-               
-                int arrAggregate = arr.
+            // Проверка формата.
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+                return;
+            }
+            // Проверка переполнения.
+            catch (OverflowException)
+            {
+                Console.WriteLine("OverflowException");
+                return;
+            }
+            // Проверка пустоты.
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("InvalidOperationException");
+                return;
+            }
 
-                int arrMyAggregate = MyClass.MyAggregate(arr);
+            // использовать синтаксис методов! SQL-подобные запросы не писать!
 
-                Console.WriteLine(arrAggregate);
-                Console.WriteLine(arrMyAggregate);
-           
+            // Множитель.
+            int mn = 1;
+
+            int arrAggregate = arr.Aggregate(delegate (int x, int y)
+            {
+                mn = -mn;
+                return x + y * mn;
+            }) + 5;
+
+            // Собственный класс.
+            int arrMyAggregate = MyClass.MyAggregate(arr);
+
+            Console.WriteLine(arrAggregate);
+            Console.WriteLine(arrMyAggregate);
+
         }
     }
 
+    /// <summary>
+    /// Собственный класс для агрегации.
+    /// </summary>
     static class MyClass
     {
-        public static int MyAggregate()
+        /// <summary>
+        /// Метод для агрегации.
+        /// </summary>
+        /// <param name="arr">Массив.</param>
+        /// <returns>Необходимое по заданию значение.</returns>
+        public static int MyAggregate(int[] arr)
         {
-            
+            // Множитель.
+            int mn = 1;
+            // Сумма.
+            int sum = 0;
+            foreach (var el in arr)
+            {
+                sum += el * mn;
+                mn = -mn;
+            }
+            return sum + 5;
         }
     }
 }
